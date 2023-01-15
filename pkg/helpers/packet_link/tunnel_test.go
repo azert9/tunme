42c359d5e87669eb1a1cebaf7c9915237ed81792
Julien Loctaux <96430950+azert9@users.noreply.github.com>
2023-01-15 18:04:32 +0100

    Improve packet link tests

--- pkg/helpers/packet_link/tunnel_test.go
+++ pkg/helpers/packet_link/tunnel_test.go
@@ -1,44 +1,125 @@
 package packet_link
 
 import (
+	"github.com/stretchr/testify/assert"
+	"io"
 	"math/rand"
 	"sync"
 	"testing"
-	"tunme/test/assert"
+	"tunme/pkg/link"
 )
 
-func TestOpenStream(t *testing.T) {
+var clientTun link.Tunnel
+var serverTun link.Tunnel
 
-	// Given
+func init() {
+
+	pipe1 := _mockPacketPipe(make(chan []byte, 100))
+	//defer close(pipe1)  // TODO
+	pipe2 := _mockPacketPipe(make(chan []byte, 100))
+	//defer close(pipe2)  // TODO
 
 	var waitGroup sync.WaitGroup
 	defer waitGroup.Wait()
 
-	pipe1 := _mockPacketPipe(make(chan []byte, 100))
-	defer close(pipe1)
-	pipe2 := _mockPacketPipe(make(chan []byte, 100))
-	defer close(pipe2)
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
+		clientTun = newTunnel(pipe1, pipe2, false)
+	}()
 
-	clientTun := newTunnel(pipe1, pipe2, false)
-	serverTun := newTunnel(pipe2, pipe1, true)
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
+		serverTun = newTunnel(pipe2, pipe1, true)
+	}()
+}
 
-	data := make([]byte, 1000)
-	rnd := rand.New(rand.NewSource(0))
-	rnd.Read(data)
+func openStream(t *testing.T) (clientStream io.ReadWriteCloser, serverStream io.ReadWriteCloser) {
 
-	// When
+	var waitGroup sync.WaitGroup
+	defer waitGroup.Wait()
 
 	waitGroup.Add(1)
 	go func() {
 		defer waitGroup.Done()
 
-		// TODO
-		_, _ = clientTun.OpenStream()
+		s, err := clientTun.OpenStream()
+		assert.NoError(t, err)
+		clientStream = s
 	}()
 
-	_, acceptErr := serverTun.AcceptStream()
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
 
-	// Then
+		s, err := serverTun.AcceptStream()
+		assert.NoError(t, err)
+		serverStream = s
+	}()
 
-	assert.NoErr(t, acceptErr)
+	return
+}
+
+func TestOpenStream(t *testing.T) {
+
+	clientStream, serverStream := openStream(t)
+	defer func() {
+		assert.NoError(t, clientStream.Close())
+		assert.NoError(t, serverStream.Close())
+	}()
+}
+
+func TestTransferDataClientToServer(t *testing.T) {
+
+	clientStream, serverStream := openStream(t)
+	defer func() {
+		assert.NoError(t, clientStream.Close())
+		assert.NoError(t, serverStream.Close())
+	}()
+
+	data := make([]byte, 10000)
+	rnd := rand.New(rand.NewSource(0))
+	rnd.Read(data)
+
+	// sending data
+
+	n, err := clientStream.Write(data)
+	assert.NoError(t, err)
+	assert.Equal(t, len(data), n)
+
+	// receiving data
+
+	receiveBuff := make([]byte, len(data))
+	_, err = io.ReadFull(serverStream, receiveBuff)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data, receiveBuff)
+}
+
+func TestTransferDataServerToClient(t *testing.T) {
+
+	clientStream, serverStream := openStream(t)
+	defer func() {
+		assert.NoError(t, clientStream.Close())
+		assert.NoError(t, serverStream.Close())
+	}()
+
+	data := make([]byte, 10000)
+	rnd := rand.New(rand.NewSource(0))
+	rnd.Read(data)
+
+	// sending data
+
+	n, err := serverStream.Write(data)
+	assert.NoError(t, err)
+	assert.Equal(t, len(data), n)
+
+	// receiving data
+
+	receiveBuff := make([]byte, len(data))
+	_, err = io.ReadFull(clientStream, receiveBuff)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data, receiveBuff)
 }
